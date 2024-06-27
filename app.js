3b603bde41f10ac560e0f44980565fe5aa67e9ae
MinorSc <misolano1192@gmail.com>
2024-06-26 20:18:25 -0600

    Feat if and else

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+
+//variables
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero por favor: ");
+let numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor: ");
 
 console.log(numeroUsuario);
-
+/*
+Este codigo 
+realiza la comparacion
+*/ 
 if(numeroUsuario == numeroSecreto) {
-    alert('Acertaste el numero')
-};
+    //Acertamos la condicion fue correcta
+    alert('Acertaste el numero');
+    //No acertamos la condicion no fue correcta
+} else {
+    alert('Lo siento no acertaste el numero' );
+}
