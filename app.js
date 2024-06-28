3514e084720b5633bf048dda62bef2533fb9dc82
MinorSc <misolano1192@gmail.com>
2024-06-27 22:06:39 -0600

    Feat Bucles

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 
 //variables
-let numeroSecreto = 8;
-let numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor: ");
+let numeroSecreto = 5;
+let numeroUsuario = 0;
+let intentos = 1;
+let palabraVeces = 'vez';
+while (numeroUsuario != numeroSecreto) {
+    let numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor: ");
 
-console.log(numeroUsuario);
-/*
-Este codigo 
-realiza la comparacion
-*/ 
-if(numeroUsuario == numeroSecreto) {
-    //Acertamos la condicion fue correcta
-    alert(`Acertaste, el numero es: ${numeroUsuario} `);
-    //No acertamos la condicion no fue correcta
-} else {
-    alert('Lo siento no acertaste el numero' );
+    console.log(numeroUsuario);
+    /*
+    Este codigo 
+    realiza la comparacion
+    */
+    if (numeroUsuario == numeroSecreto) {
+        //Acertamos la condicion fue correcta
+            alert(`Acertaste, el numero es: ${numeroUsuario} Lo hiciste en ${intentos} ${palabraVeces}`);
+            //No acertamos la condicion no fue correcta
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+            alert('El numero secreto es menor');
+        } else {
+            alert('El numero secreto es mayor');
+        }
+        // incrementamos el contador cuando la persona no acierta 
+        intentos = intentos + 1;
+        palabraVeces = 'veces';
+
+        // alert('Lo siento no acertaste el numero');
+
+    }
 }
