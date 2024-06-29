c9495e34bfb2a63565e9fc3380f4acf52143f8e7
MinorSc <misolano1192@gmail.com>
2024-06-28 22:30:28 -0600

    Feat Js1

---
+++ Logica2/app2.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego de numero secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'India un numero del 1 al 10';
+
+function intentoDeUsuario() {
+    alert('Click desde el botón')
+}
